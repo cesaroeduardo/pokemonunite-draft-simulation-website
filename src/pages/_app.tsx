149dd83f15ea883d086ff9d0fa37c80b65ec08f1
RAVI <rafael.raiol@gmail.com>
2023-04-16 02:11:37 -0300

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app'
 
 import { ThemeProvider } from "@material-tailwind/react";
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+
+      <Analytics />
     </ThemeProvider>
   )
 }
